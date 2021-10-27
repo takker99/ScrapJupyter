171a96e42ae068a2f9e21b5feeedae5fef5ca9f2
takker99 <37929109+takker99@users.noreply.github.com>
2021-10-27 19:53:00 +0900

    :+1: bypass throttle

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,12 @@ import { bundle, isAvailableExtensions } from "./bundler.ts";
 import { eventName } from "./deps/scrapbox.ts";
 import type { Scrapbox } from "./deps/scrapbox.ts";
 import { execMenu } from "./components/execMenu.ts";
+import { throttle } from "./deps/throttle.ts";
 declare const scrapbox: Scrapbox;
 
 const menus = [] as ReturnType<typeof execMenu>[];
 
-const callback = () => {
+const update = async () => {
   const files = getCodeFiles();
   // ボタンを全部リセットする
   menus.forEach(({ menu, setStatus }) => {
@@ -44,6 +45,8 @@ const callback = () => {
       line?.insertBefore?.(menu, line?.firstElementChild);
     });
   });
+  await Promise.resolve();
 };
-callback();
+const callback = throttle(update);
+await callback();
 scrapbox.addListener("lines:changed" as eventName, callback);

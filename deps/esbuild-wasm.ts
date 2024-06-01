854405622b3a7a1720c653b54bb2285d5d5d086d
GitHub <noreply@github.com>
2024-06-01 00:57:42 +0000

    :arrow_up: update deno dependencies

--- deps/esbuild-wasm.ts
+++ deps/esbuild-wasm.ts
@@ -1,5 +1,5 @@
 export {
   build,
   initialize,
-} from "https://raw.githubusercontent.com/takker99/esbuild-wasm-no-blob/0.20.1/mod.ts";
-export * from "https://raw.githubusercontent.com/takker99/esbuild-wasm-no-blob/0.20.1/types.ts";
+} from "https://raw.githubusercontent.com/takker99/esbuild-wasm-no-blob/0.21.4/mod.ts";
+export * from "https://raw.githubusercontent.com/takker99/esbuild-wasm-no-blob/0.21.4/types.ts";

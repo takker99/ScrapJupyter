e4e537a420d945c722db80d4661d2dd6d026b126
takker99 <37929109+takker99@users.noreply.github.com>
2024-07-30 18:53:18 +0900

    refactor: Re-export all `esbuild-wasm` identifiers

--- deps/esbuild-wasm.ts
+++ deps/esbuild-wasm.ts
@@ -1,6 +1,10 @@
 export {
   analyzeMetafile,
   build,
+  formatMessages,
   initialize,
+  stop,
+  transform,
+  version,
 } from "https://raw.githubusercontent.com/takker99/esbuild-wasm-no-blob/0.21.5/mod.ts";
 export * from "https://raw.githubusercontent.com/takker99/esbuild-wasm-no-blob/0.21.5/types.ts";
